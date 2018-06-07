50cf084a9d311a840f8f3adc49a104fe28b95235
bugmain <uscostudio@gmail.com>
2018-06-07 01:18:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sebastian/uscogym/Ubicacion1.java
@@ -0,0 +1,60 @@
+package com.example.sebastian.uscogym;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Ubicacion1 extends Fragment implements OnMapReadyCallback {
+
+
+    GoogleMap map;
+
+    public Ubicacion1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_ubicacion1, container, false);
+
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng pp = new LatLng(2.941948, -75.298523);
+        MarkerOptions options = new MarkerOptions();
+        options.position(pp).title("Usco Gym");
+        map.addMarker(options);
+        map.moveCamera(CameraUpdateFactory.newLatLng(pp));
+
+    }
+}
